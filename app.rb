2f442e8089380ee6d980a9d1681775c6d732a95a
Dan Gyi <daniel.gyi@gmail.com>
2019-09-03 18:27:38 +0100

    creates name form and prints names

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 
 class Battle < Sinatra::Base
+  
   get '/' do
-    "Let's battle!"
+    erb(:index)
+  end
+
+  post '/names' do
+    @name1 = params[:player_1_name]
+    @name2 = params[:player_2_name]
+    erb(:play)
   end
 
   !run if app_file == $0
+
 end
--- spec/features/app_feature_spec.rb
+++ spec/features/app_feature_spec.rb
@@ -1,6 +1,9 @@
-feature "testing infrastructure" do
-  scenario "can run app and check page content" do
-    visit ('/')
-    expect(page).to have_content "Let's battle!"
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave vs. Mittens'
   end
 end
