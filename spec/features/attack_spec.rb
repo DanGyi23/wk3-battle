fb4944d77f002a37301f2113fd391e8f829efbb3
Dan Gyi <daniel.gyi@gmail.com>
2019-09-04 11:27:57 +0100

    add switch turns and implement with tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,9 +1,32 @@
 require "spec_helper"
 
-feature 'Attack player' do
+feature 'Attack' do
+  before(:each) do 
+    sign_in_and_play
+  end
+  
   scenario 'Player 1 attacks player 2' do
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
+
+  scenario 'reduce player 2 HP by 10' do
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content('Mittens: 100HP')
+    expect(page).to have_content('Mittens: 90HP')
+  end
+
+  scenario 'reduce Player 1 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content 'Dave attacked Mittens'
+    click_button 'OK'
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content('Dave: 100HP')
+    expect(page).to have_content('Dave: 90HP')
   end
+
 end
--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -4,6 +4,8 @@ feature 'switch turns' do
   scenario 'after an attack, the game switches turns from p1 to p2' do
     sign_in_and_play
     click_button 'Attack'
+    click_button "OK"
+    expect(page).not_to have_content("Dave, it is now your turn")
     expect(page).to have_content("Mittens, it is now your turn")
   end
 end
