9cd7b00c75af8b340395ab7314d765326118863b
Dan Gyi <daniel.gyi@gmail.com>
2019-09-04 10:08:25 +0100

    updates controller to use , refactor tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,20 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  subject(:dave) { Player.new('Dave') }
-  subject(:mittens) { Player.new('Mittens') }
+  subject(:game) { described_class.new(player_1,player_2) }
+  subject(:player_1) { double :player }
+  subject(:player_2) { double :player }
 
-  describe '#attack' do
-    it 'damages the player' do
-      expect(mittens).to receive(:receive_damage)
-      game.attack(mittens)
+  describe '#player1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq(player_1)
     end
   end
+
+  describe '#player2' do
+    it 'retrieves the first player' do
+      expect(game.player_2).to eq(player_2)
+    end
+  end
+
 end
